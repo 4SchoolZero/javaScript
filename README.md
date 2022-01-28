0f205488c7497ee14cc9f40f70e5d37362630678
Tijmen <34179@ma-web.nl>
2022-01-28 13:48:25 +0100

    tentoonstelling fix

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # javaScript
-## https://34179.hosts1.ma-cloud.nl/seizoenen/
-## http://34179.hosts1.ma-cloud.nl/slideshow/
-## https://34179.hosts1.ma-cloud.nl/tentoonstelling/
-## http://34179.hosts1.ma-cloud.nl/boodschappen/
+
+## [jsElements]( http://34179.hosts1.ma-cloud.nl/jsElements/)
+## [Seizoenen]( https://34179.hosts1.ma-cloud.nl/seizoenen/)
+## [SlideShow]( http://34179.hosts1.ma-cloud.nl/slideshow/)
+## [TentoonStelling]( https://34179.hosts1.ma-cloud.nl/tentoonstelling/)
+## [Boodschappen]( http://34179.hosts1.ma-cloud.nl/boodschappen/)
--- tentoonstelling/script.js
+++ tentoonstelling/script.js
@@ -1,15 +1,36 @@
-const img = document.getElementById("pic");
-const txt = document.getElementById("textImg");
-let imges = ["source/painting0.jpg", "source/painting1.jpg", "source/painting2.jpg", "source/painting3.jpg", "source/painting4.jpg", "source/painting5.jpg"]
-let texts = ["", "De rode wijngaard november 1888 Pesjkinmuseum Moskouw", "De sterrennacht juni 1889 Museum of Modern Art New York", "Sterrennacht boven de Rh�ne september 1888 Mus�e d'Orsay Parijs", "Zonnebloemen januari 1889 Neue Pinakothek Munchen", "Boerderij in de Provence september 1888 National Gallery of Art Washington D.C."]
-x = 0
+let imges = [
+    "source/painting0.jpg", 
+    "source/painting1.jpg", 
+    "source/painting2.jpg", 
+    "source/painting3.jpg", 
+    "source/painting4.jpg", 
+    "source/painting5.jpg"
+];
+let texts = [
+    "", 
+    "De rode wijngaard november 1888 Pesjkinmuseum Moskouw", 
+    "De sterrennacht juni 1889 Museum of Modern Art New York", 
+    "Sterrennacht boven de Rh�ne september 1888 Mus�e d'Orsay Parijs", 
+    "Zonnebloemen januari 1889 Neue Pinakothek Munchen", 
+    "Boerderij in de Provence september 1888 National Gallery of Art Washington D.C."
+];
+x = 0;
+
 function changeIMG(){
-    x++
-    txt.innerHTML = texts[x]
-    img.src = imges[x]
+    if(x >= 5){
+        x = 0;
+    }
+    x++;
+    document.getElementById("textImg").innerHTML = texts[x];
+    document.getElementById("pic").src = imges[x];
+    console.log(x);
 }
 function changeIMGd(){
-    x--
-    txt.innerHTML = texts[x]
-    img.src = imges[x]
+    if(x == 0){
+        x = 6;
+    }
+    x--;
+    document.getElementById("textImg").innerHTML = texts[x];
+    document.getElementById("pic").src = imges[x];
+    console.log(x);
 }
